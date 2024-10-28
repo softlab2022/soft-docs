25e1c39cd2028a6b07ec79becef1af17ccb1e388
Prince Ahmed <hitprince99@gmail.com>
2024-10-28 11:48:15 +0600

    fix docs query

--- templates/docs.php
+++ templates/docs.php
@@ -4,101 +4,111 @@ get_header();
 
 ?>
 
-<div class="softdocs-wrap softdocs-archive-wrap">
+    <div class="softdocs-wrap softdocs-archive-wrap">
 
-	<?php softdocs_get_template('search-form'); ?>
+		<?php softdocs_get_template( 'search-form' ); ?>
 
-	<div class="softdocs-archive-main">
-		<?php
+        <div class="softdocs-archive-main">
+			<?php
 
-		$categories = get_terms('docs_category', array(
-			'hide_empty' => false,
-			'parent'   => 0,
-			'meta_key' 	=> 'order',
-			'orderby' 	=> 'meta_value_num',
-			'order'		=> 'ASC'
-		));
-
-		if (!empty($categories)) {
-			$category_count = 0;
-			foreach ($categories as $category) {
-				if ($category_count >= 4) break;
-				$name        = $category->name;
-				$slug        = $category->slug;
-				$count       = $category->count;
-				$description = $category->description;
-				$term_id     = $category->term_id;
-				$term_link   = get_term_link($category);
-				$image_id    = get_term_meta($term_id, 'category-image-id', true);
-
-				$args = array(
-					'post_type'      => 'docs',
-					'posts_per_page' => 10,
-					'orderby'        => array(
-						'menu_order' => 'ASC',
-						'date'       => 'ASC',
+			$categories = get_terms( 'docs_category', array(
+				'hide_empty' => false,
+				'parent'     => 0,
+				'meta_key'   => 'order',
+				'orderby'    => 'meta_value_num',
+				'order'      => 'ASC',
+				'meta_query' => array(
+					array(
+						'key'     => 'order',
+						'value'   => '',
+						'compare' => '!=',
 					),
-					'order'          => 'ASC',
-					'tax_query'      => array(
-						array(
-							'taxonomy' => 'docs_category',
-							'field'    => 'slug',
-							'terms'    => $slug,
+				),
+			) );
+
+			if ( ! empty( $categories ) ) {
+				$category_count = 0;
+				foreach ( $categories as $category ) {
+					if ( $category_count >= 4 ) {
+						break;
+					}
+					$name        = $category->name;
+					$slug        = $category->slug;
+					$count       = $category->count;
+					$description = $category->description;
+					$term_id     = $category->term_id;
+					$term_link   = get_term_link( $category );
+					$image_id    = get_term_meta( $term_id, 'category-image-id', true );
+
+					$args = array(
+						'post_type'      => 'docs',
+						'posts_per_page' => 10,
+						'orderby'        => array(
+							'menu_order' => 'ASC',
+							'date'       => 'ASC',
 						),
-					),
-				);
+						'order'          => 'ASC',
+						'tax_query'      => array(
+							array(
+								'taxonomy' => 'docs_category',
+								'field'    => 'slug',
+								'terms'    => $slug,
+							),
+						),
+					);
 
-				$query = new WP_Query($args);
+					$query = new WP_Query( $args );
 
-				if ($query->have_posts()) {
+					if ( $query->have_posts() ) {
 
-		?>
-					<div class="softdocs-cat">
+						?>
+                        <div class="softdocs-cat">
 
-						<div class="softdocs-cat-title">
-							<a href="<?php echo $term_link; ?>">
+                            <div class="softdocs-cat-title">
+                                <a href="<?php echo $term_link; ?>">
+									<?php
+									if ( $image_id ) {
+										echo wp_get_attachment_image( $image_id );
+									}
+									?>
+                                    <span class="category-name"><?php echo $name ?></span>
+                                    <span class="docs-count"><?php echo $count; ?></span>
+                            </div>
+
+                            <div class="docs-list">
 								<?php
-								if ($image_id) {
-									echo wp_get_attachment_image($image_id);
+
+								while ( $query->have_posts() ) {
+									$query->the_post();
+									$post_id = get_the_ID();
+									$title   = get_the_title();
+									$link    = get_the_permalink();
+									$image   = get_the_post_thumbnail_url( $post_id, 'full' );
+									?>
+                                    <a href="<?php echo $link; ?>" class="docs-list-item">
+                                        <img class="doc-icon" src="<?php echo SOFT_DOCS_ASSETS; ?>/images/doc-icon.png"
+                                             alt="doc">
+                                        <span class="doc-title"><?php echo $title; ?></span>
+                                    </a>
+									<?php
 								}
+
+								wp_reset_postdata();
+
 								?>
-								<span class="category-name"><?php echo $name ?></span>
-								<span class="docs-count"><?php echo $count; ?></span>
-						</div>
+                            </div>
 
-						<div class="docs-list">
-							<?php
+                            <a href="<?php echo $term_link; ?>" class="softdocs-cat-btn">Explore More</a>
 
-							while ($query->have_posts()) {
-								$query->the_post();
-								$post_id = get_the_ID();
-								$title   = get_the_title();
-								$link    = get_the_permalink();
-								$image   = get_the_post_thumbnail_url($post_id, 'full');
-							?>
-								<a href="<?php echo $link; ?>" class="docs-list-item">
-									<img class="doc-icon" src="<?php echo SOFT_DOCS_ASSETS; ?>/images/doc-icon.png" alt="doc">
-									<span class="doc-title"><?php echo $title; ?></span>
-								</a>
-							<?php
-							}
-
-							wp_reset_postdata();
-
-							?>
-						</div>
-
-						<a href="<?php echo $term_link; ?>" class="softdocs-cat-btn">Explore More</a>
-
-					</div>
-		<?php }
-				$category_count++;
+                        </div>
+					<?php }
+					$category_count ++;
+				}
 			}
-		}
 
-		?>
-	</div>
-</div>
+			?>
+        </div>
+    </div>
 
 <?php
 get_footer();
